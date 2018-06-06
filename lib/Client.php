6a278a74e5d1b8fa8e50f5bd3974fc997c287dba
Alex Agafonov <awa77@mail.ru>
2018-06-06 06:49:13 +0300

    Initial commit

---
+++ lib/Client.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex Agafonov
+ * Date: 06.06.18
+ * Time: 5:43
+ */
+
+/**
+ * The MIT License
+ *
+ * Copyright (c) 2017 Paymaster LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace PaymasterSdkPHP;
+
+class Client {
+
+    public function __construct(Client\ApiClientInterface $apiClient = null, ConfigurationLoaderInterface $configLoader = null)
+    {
+
+        if ($apiClient === null) {
+            $apiClient = new Client\CurlClient();
+        }
+
+        if ($configLoader === null) {
+            $configLoader = new ConfigurationLoader();
+            $config       = $configLoader->load()->getConfig();
+            $this->setConfig($config);
+            $apiClient->setConfig($config);
+        }
+        $this->attempts  = self::DEFAULT_ATTEMPTS_COUNT;
+        $this->apiClient = $apiClient;
+
+    }
+
+
+}
